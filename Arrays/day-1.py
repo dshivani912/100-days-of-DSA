7e9ac70b6da5eceb0f2e8f8977eacf7ad6f9c341
Deveri Shivani <95647550+dshivani912@users.noreply.github.com>
2023-08-08 00:38:32 +0530

    Update day-1.py
    Reverse an array

--- Arrays/day-1.py
+++ Arrays/day-1.py
@@ -1,1 +1,15 @@
+#Reverse an array
 
+def reverse_an_array(a):
+    start=0;end=len(a)-1;
+    while(start<=len(a)//2):
+        a[start],a[end]=a[end],a[start]
+        start=start+1
+        end=end-1
+    return a
+
+a=[1,2,3,4,5]
+ans=reverse_an_array(a)
+print(a)
+
+#time complexity is O(n) and space complexity is O(1)
