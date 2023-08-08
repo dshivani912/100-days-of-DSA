5ecd57ed82e334e2a7d9edc4c6ed6b546c4d50a8
Deveri Shivani <95647550+dshivani912@users.noreply.github.com>
2023-08-08 16:31:10 +0530

    Update kth_element.java

--- Arrays/day-2/kth_element.java
+++ Arrays/day-2/kth_element.java
@@ -1,1 +1,18 @@
+//Find the "Kth" max and min element of an array 
 
+
+import java.util.*;
+public class kth_element{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int size=sc.nextInt();
+        int k=sc.nextInt();
+        int[] array=new int[size];
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.print(array[k-1]+" ");
+        System.out.println(array[size-k]);
+    }
+}
