cc3376e2dfd9e23f0f1542ebf13156397d43a579
Deveri Shivani <95647550+dshivani912@users.noreply.github.com>
2023-08-08 23:18:04 +0530

    Create array_with_0's_and_1's.py

---
+++ Arrays/day-2/array_with_0's_and_1's.py
@@ -0,0 +1,47 @@
+#Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+
+def sort_array_0s_1s_2s(A,n):  
+  i=0  
+  low = 0  
+  mid = 0  
+  high = n-1  
+  while (mid<=high):  
+    if (A[mid]==0):
+      print("a[mid]==0")  
+      print(mid,low,high)
+      
+      temp=A[mid]  
+      A[mid]=A[low]  
+      A[low]=temp  
+      low = low + 1  
+      mid = mid + 1
+      print(A)
+      
+    elif (A[mid]==1):
+      print("a[mid]==1")  
+      print(mid,low,high)
+      mid = mid + 1
+      print(A)
+    else:
+      print("a[mid]==2")  
+      print(mid,low,high)  
+      temp=A[mid]  
+      A[mid]=A[high]  
+      A[high]=temp  
+      high = high - 1
+      print(A)
+  
+print("Enter size of an array: ")  
+n=int(input())  
+print("Enter array elements: \n")  
+i=0  
+A=[]  
+while(i<n):  
+  ele=int(input())  
+  A.append(ele)  
+  i=i+1  
+print("Array after sorting: \n")  
+sort_array_0s_1s_2s(A,n)  
+i=0  
+for i in A:  
+  print(i) 
