dd01d46bedef7a43b9a6f765b67937133a860b06
Deveri Shivani <95647550+dshivani912@users.noreply.github.com>
2023-08-12 19:59:11 +0530

    Update cyclic_array.py

--- Arrays/day-4/cyclic_array.py
+++ Arrays/day-4/cyclic_array.py
@@ -1,1 +1,8 @@
-
+def rotate( arr, n):
+    temp=arr[0:n-1]
+    del arr[0:n-1]
+    for i in range(len(temp)):
+        arr.append(temp[i])
+    return arr
+arr=list(map(int,input().split()))
+rotate(arr,len(arr))
